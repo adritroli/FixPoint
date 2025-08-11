0901174f52132d6570113defa5ece124c1c9d14f
Adrian <adrianfernandez308@gmail.com>
2025-08-11 19:11:37 -0300

    add theme provider

--- FixPoint-Git/src/App.tsx
+++ FixPoint-Git/src/App.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
+      <ModeToggle />
       <h1 className="text-3xl font-bold">Hello, world!</h1>
       <p className="text-lg">This is a simple example of a React component.</p>
       <Button>Click me</Button>
