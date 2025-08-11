c0c011cc38a614909dbb7a600f6a240854b7db36
Adrian <adrianfernandez308@gmail.com>
2025-08-11 19:03:19 -0300

    cli de shadcn ui

---
+++ FixPoint-Git/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello, world!</h1>
+      <p className="text-lg">This is a simple example of a React component.</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
